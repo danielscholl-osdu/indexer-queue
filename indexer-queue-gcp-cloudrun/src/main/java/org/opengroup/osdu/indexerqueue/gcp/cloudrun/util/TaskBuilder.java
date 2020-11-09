d73506cdb86ceef386b20ebb8dd8de40280cd3e4
Rustam_Lotsmanenko <Rustam_Lotsmanenko@epam.com>
2020-11-09 20:57:33 +0400

    GONRG-1041 Indexer-queue should create HTTP Target Cloud Tasks

---
+++ indexer-queue-gcp-cloudrun/src/main/java/org/opengroup/osdu/indexerqueue/gcp/cloudrun/util/TaskBuilder.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2020 Google LLC
+ * Copyright 2020 EPAM Systems, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opengroup.osdu.indexerqueue.gcp.cloudrun.util;
+
+
+import com.google.cloud.tasks.v2beta3.Task;
+import java.io.IOException;
+import org.opengroup.osdu.core.common.model.search.CloudTaskRequest;
+
+public interface TaskBuilder {
+
+	Task createTask(CloudTaskRequest request) throws IOException;
+}
