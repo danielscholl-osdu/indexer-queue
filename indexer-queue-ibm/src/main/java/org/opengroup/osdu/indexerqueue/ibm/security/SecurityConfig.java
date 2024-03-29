788d122f16a5633e79129d96c45f79b973777b9a
IshaKumari <isha.kumari@ibm.com>
2024-03-29 12:01:36 +0530

    Fix Crashloop_issue

--- indexer-queue-ibm/src/main/java/org/opengroup/osdu/indexerqueue/ibm/security/SecurityConfig.java
+++ indexer-queue-ibm/src/main/java/org/opengroup/osdu/indexerqueue/ibm/security/SecurityConfig.java
@@ -14,8 +14,6 @@
 
 package org.opengroup.osdu.indexerqueue.ibm.security;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -25,16 +23,21 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
- 
     @Bean
     public SecurityFilterChain defaultFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
-                .anonymous();
+            .cors(AbstractHttpConfigurer::disable)
+            .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
+            .httpBasic(withDefaults());
         return http.build();
     }
 }
+
