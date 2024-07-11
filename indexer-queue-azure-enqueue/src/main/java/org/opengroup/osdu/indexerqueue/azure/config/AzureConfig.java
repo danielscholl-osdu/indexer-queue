c75f7d9cfef453c7e92d411f7d7c3970fb324a0d
VidyaDharani Lokam <vidyadharani_lokam@epam.com>
2024-07-11 09:24:21 +0000

    [MS-43510] remediate tomcat vulnerability

--- indexer-queue-azure-enqueue/src/main/java/org/opengroup/osdu/indexerqueue/azure/config/AzureConfig.java
+++ indexer-queue-azure-enqueue/src/main/java/org/opengroup/osdu/indexerqueue/azure/config/AzureConfig.java
@@ -3,16 +3,9 @@ package org.opengroup.osdu.indexerqueue.azure.config;
 import com.microsoft.applicationinsights.TelemetryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import java.time.Duration;
-
 
 @Configuration
 public class AzureConfig {
-
-    @Bean
-    public Duration slowIndicatorLoggingThreshold() {
-        return Duration.ofSeconds(5);
-    }
 
     @Bean
     public TelemetryClient telemetryClient() {
